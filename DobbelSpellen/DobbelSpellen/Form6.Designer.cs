19d32f0dbda223c6311199c1599dd2c315e88b24
lhhamming <luuk.hamming@gmail.com>
2021-04-02 20:12:08 +0200

    Mooi ding niet

--- DobbelSpellen/DobbelSpellen/Form6.Designer.cs
+++ DobbelSpellen/DobbelSpellen/Form6.Designer.cs
@@ -29,12 +29,33 @@ namespace DobbelSpellen
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(71, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(626, 57);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "HQS Is een jong dynamisch bedrijf wat zich specialiceert in de ict. We werken met" +
+    " C#,Javascript en html ook werken we via Bootstrap zodat uw websites net een stu" +
+    "k vloeienbaarder is";
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form6";
             this.Text = "Form6";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
